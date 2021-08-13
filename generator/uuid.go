f465c77eeffbcd7ab1da65014954d3a1fb0dac64
Mateus Souza <mateussouzaweb@gmail.com>
2021-08-13 16:45:18 -0300

    Better method description

--- generator/uuid.go
+++ generator/uuid.go
@@ -6,7 +6,7 @@ import (
 	"github.com/google/uuid"
 )
 
-// UUID method
+// UUID generates a new ramdom UUID without dashes
 func UUID() string {
 	return strings.ReplaceAll(uuid.New().String(), "-", "")
 }
