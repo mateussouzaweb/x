2ed05188134ba6b00406f8cd6e05a93e4b29e50d
Mateus Souza <mateussouzaweb@gmail.com>
2025-05-09 20:46:02 +0000

    Feat: add flag parsing methods

---
+++ cli/arguments.go
@@ -0,0 +1,80 @@
+package cli
+
+import (
+	"slices"
+	"strconv"
+	"strings"
+)
+
+// Retrieve argument with given keys or in given index position
+func Arg(args []string, keys string, defaultValue string) string {
+
+	options := strings.Split(keys, ",")
+	positional := map[string]string{}
+	counter := 0
+
+	for _, arg := range args {
+		// Check for --key=value -key=value or key=value like format first
+		for _, key := range options {
+			if strings.HasPrefix(arg, key+"=") {
+				return strings.TrimPrefix(arg, key+"=")
+			}
+		}
+
+		// If none of the options match, then put value into positional list
+		if !strings.HasPrefix(arg, "-") {
+			positional[strconv.Itoa(counter)] = arg
+			counter++
+		}
+	}
+
+	// Check positional index format, valid only for numeric keys
+	for _, index := range options {
+		if _, err := strconv.Atoi(index); err != nil {
+			continue
+		}
+		if value, ok := positional[index]; ok {
+			return value
+		}
+	}
+
+	return defaultValue
+}
+
+// Retrieve boolean flag with given keys
+func Flag(args []string, keys string, defaultValue bool) bool {
+
+	options := strings.Split(keys, ",")
+
+	// Check for --key or -key like format
+	for _, arg := range args {
+		if slices.Contains(options, arg) {
+			return true
+		}
+	}
+
+	return defaultValue
+}
+
+// Retrieve multiple values for arguments with given key
+func Multiple(args []string, key string) []string {
+	result := []string{}
+
+	for {
+		// Find next argument key details
+		item := Arg(args, key, "")
+
+		// Break when no more items found
+		// When found, remove key details from arguments list
+		// Also append to the list of items
+		if item == "" {
+			break
+		} else {
+			index := slices.Index(args, key+"="+item)
+			args = append(args[:index], args[index+1:]...)
+			result = append(result, item)
+		}
+	}
+
+	return result
+}
